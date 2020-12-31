cb5df30257389e204b8c0ef9dcf12e497a97ccba
spiritbro1 <rinoakbr@gmail.com>
2020-12-31 08:58:04 +0700

    blacklist done

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-{
+module.exports={
   "development": {
     "username": "panda",
     "password": "panda1234",
@@ -14,10 +14,10 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
   }
 }
